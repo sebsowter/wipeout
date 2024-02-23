baeec09e18c6d7caf3b14932bcad625cbc9ecb0b
Seb Sowter <seb@sebsowter.com>
2024-02-23 22:07:22 +0000

    Progress

--- main.ts
+++ main.ts
@@ -10,7 +10,7 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
+const geometry = new THREE.BoxGeometry(1, 0.5, 1);
 const geometry2 = new THREE.BoxGeometry(0.5, 0.5, 0.5);
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 const material2 = new THREE.MeshBasicMaterial({ color: 0x0000ff });
@@ -39,68 +39,23 @@ function updateCamera() {
   const time = clock.getElapsedTime();
   const looptime = 20;
   const t = (time % looptime) / looptime;
-  const t2 = ((time + 0.1) % looptime) / looptime;
-  const t3 = ((time + 0.101) % looptime) / looptime;
+  //const t2 = ((time + 0.1) % looptime) / looptime;
+  //const t3 = ((time + 0.101) % looptime) / looptime;
 
-  const pos = path.getPointAt(t);
-  const pos2 = path.getPointAt(t2);
-  // const pos3 = path.getPointAt(t3);
-
+  const position = path.getPointAt(t);
   const tangent = path.getTangent(t).normalize();
 
-  const axis = tangent; // .crossVectors(new THREE.Vector3(0, 0, 1), tangent).normalize();
+  const up = new THREE.Vector3(0, 1, 0);
+  const axis = new THREE.Vector3().crossVectors(up, tangent).normalize();
+  const radians = Math.acos(up.dot(tangent));
 
-  actor.position.copy(pos);
-
-  const radians = Math.acos(new THREE.Vector3(0, 0, 1).dot(tangent));
-
+  actor.position.copy(position);
   actor.quaternion.setFromAxisAngle(axis, radians);
 
-  //const angle = actor.position.angleTo(lookTarget.position);
-  //console.log("angle", angle);
+  const cameraPosition = actor.position.clone();
+  cameraPosition.add(new THREE.Vector3(0, 0, 1).multiplyScalar(20));
 
-  //actor.getWorldQuaternion()
-
-  // const quaternion = new THREE.Quaternion();
-  //quaternion.set;
-  //quaternion.setFromAxisAngle(new THREE.Vector3(0, 0, 1), tangent.y);
-  //console.log("x", tangent.x);
-  console.log("y", tangent.y);
-
-  // actor.setRotationFromQuaternion(quaternion);
-  //actor.lookAt(new THREE.Vector3(pos.x, pos.y, -10));
-  //actor.applyQuaternion(quaternion);
-
-  //actor.applyQuaternion(quaternion)
-
-  //const offset = new THREE.Vector3(0, 0, Math.PI / 2).applyQuaternion(quaternion);
-  //.applyAxisAngle(new THREE.Vector3(0, 0, 1).normalize(), actor.rotation.z);
-  //const vectorss = vector.add(offset);
-  //camera.setRotationFromAxisAngle()
-  //const ss = actor.position.clone().add(cameraTarget.position);
-  //camera.position.set(ss.x, ss.y, ss.z);
-  //camera.applyQuaternion(quaternion); // Apply Quaternion
-
-  // camera.quaternion.normalize(); // Normalize Quaternion
-  //camera.rotateOnAxis(new THREE.Vector3(0, 1, 0), 0);
-  //camera.rotateOnAxis(new THREE.Vector3(1, 0, 1), 0);
-
-  // this.dir.set(0, 0, 1);
-  // this.up.set(0, 1, 0);
-
-  //this.targetObject.matrix.rotateAxis(this.up);
-  //this.targetObject.matrix.rotateAxis(this.dir);
-
-  //this.speedOffset += (this.speedOffsetMax * ratio - this.speedOffset) * Math.min(1, 0.3 * dt);
-
-  const pos22 = actor.position.clone();
-  //this.target.copy(this.targetObject.position);
-  //this.target.subSelf(this.dir.multiplyScalar(this.zoffset + this.speedOffset));
-  //pos22.add(new THREE.Vector3(0, 1, 0).multiplyScalar(5).applyQuaternion());
-  pos22.add(new THREE.Vector3(0, 0, 1).multiplyScalar(20));
-  //this.target.y += -this.up.y + this.yoffset;
-  camera.position.copy(pos22);
-
+  camera.position.copy(cameraPosition);
   camera.lookAt(actor.position);
 }
 
