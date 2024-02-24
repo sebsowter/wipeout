77de75b035859b49430d923a21c41e691a165128
Seb Sowter <seb@sebsowter.com>
2024-02-24 20:54:09 +0000

    Progress

--- main.ts
+++ main.ts
@@ -15,7 +15,7 @@ const cameraMode: CameraModes = CameraModes.PLAYER;
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-const geometry = new THREE.BoxGeometry(0.5, 1, 0.5);
+const geometry = new THREE.BoxGeometry(0.5, 0.5, 1);
 const geometry2 = new THREE.BoxGeometry(0.5, 0.5, 0.5);
 
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
@@ -24,10 +24,10 @@ const material2 = new THREE.MeshBasicMaterial({ color: 0x0000ff });
 const cube = new THREE.Mesh(geometry, material);
 const cube1 = new THREE.Mesh(geometry2, material2);
 const cube2 = new THREE.Mesh(geometry2, material2);
-cube1.position.setY(-4);
-cube1.position.setZ(2);
-cube2.position.setY(-2);
-cube2.position.setZ(1);
+cube1.position.setZ(-4);
+cube1.position.setY(2);
+cube2.position.setZ(-2);
+cube2.position.setY(1);
 
 const [flows, path] = road();
 
@@ -52,6 +52,7 @@ const pos2 = new THREE.Vector3();
 const up = new THREE.Vector3();
 const offset = new THREE.Vector3();
 const quaternion = new THREE.Quaternion();
+const dummy = new THREE.Object3D();
 
 for (let i = 0; i < 999; i++) {
   let p1 = path.getPointAt(i / 1000);
@@ -84,14 +85,14 @@ function updateCamera() {
   const time = clock.getElapsedTime();
   const looptime = 20;
   const point = (time % looptime) / looptime;
-  const t2 = ((time + 0.1) % looptime) / looptime;
+  const t2 = ((time + 0.001) % looptime) / looptime;
   //const t3 = ((time + 0.101) % looptime) / looptime;
 
-  const position = path.getPointAt(point);
-  const position2 = path.getPointAt(point);
-  const tangent = path.getTangentAt(point).normalize();
-  const [axis, radians] = getAxisAngle(new THREE.Vector3(0, 1, 0), tangent);
-  const [axis2, radians2] = getAxisAngle(new THREE.Vector3(1, 0, 0), tangent);
+  path.getPointAt(point, pos1);
+  path.getPointAt(t2, pos2);
+  //const tangent = path.getTangentAt(point).normalize();
+  //const [axis, radians] = getAxisAngle(new THREE.Vector3(0, 1, 0), tangent);
+  //const [axis2, radians2] = getAxisAngle(new THREE.Vector3(1, 0, 0), tangent);
 
   //path.arcLengthDivisions = 100 / 2;
   //path.updateArcLengths();
@@ -99,8 +100,9 @@ function updateCamera() {
   //const points = path.getSpacedPoints(100);
   //const frenetFrames = path.computeFrenetFrames(100, true);
 
-  actor.position.copy(position);
-  actor.setRotationFromAxisAngle(axis, radians);
+  actor.position.copy(pos1);
+  actor.lookAt(pos2);
+  //actor.setRotationFromAxisAngle(axis, radians);
   //actor.setRotationFromAxisAngle(axis2, radians2);
   //actor.quaternion.set(actor.quaternion.x, 0, actor.quaternion.z, actor.quaternion.w);
   console.log("actor.quaternion", actor.quaternion);
@@ -108,29 +110,37 @@ function updateCamera() {
 
   // ----------------------------------
 
+  /*
   path.getPointAt(point, pos1);
   path.getPointAt(t2, pos2);
 
   up.set(0, 1, 0);
   forward.subVectors(pos1, pos2).normalize();
   right.crossVectors(up, forward).normalize();
-  //up.crossVectors(forward, right);
+  up.crossVectors(forward, right);
 
-  const angle = Math.atan2(-forward.x, -forward.y);
+  const angle = Math.atan2(-forward.x, -forward.z);
   quaternion.setFromAxisAngle(up, angle);
 
+  dummy.quaternion.setFromAxisAngle(up, angle);
+  */
+
+  //actor.rotation.z = (180 * Math.PI) / 180 - dummy.rotation.z;
+  //cube.rotation.z = (180 * Math.PI) / 180 - dummy.rotation.z;
   // left side of track
-  vector10.copy(offset.set(-0.2, 0, 0));
-  vector10.applyQuaternion(quaternion);
-  vector10.add(pos1);
+  //vector10.copy(offset.set(-0.2, 0, 0));
+  //vector10.applyQuaternion(quaternion);
+  //vector10.add(pos1);
 
   // right side of track
-  vector11.copy(offset.set(0.2, 0, 0));
-  vector11.applyQuaternion(quaternion);
-  vector11.add(pos1);
+  //vector11.copy(offset.set(0.2, 0, 0));
+  //vector11.applyQuaternion(quaternion);
+  //vector11.add(pos1);
 
+  //actor.applyQuaternion(quaternion);
   //actor.matrix.lookAt(vector10, vector11, up);
 
+  //actor.quaternion.copy(quaternion);
   //actor.quaternion.setFromRotationMatrix(actor.matrix);
   //actor.setRotationFromAxisAngle(axis, radians);
   //actor.position.copy(pos1);
@@ -145,7 +155,7 @@ function updateCamera() {
 
     case CameraModes.BIRD:
     default:
-      const cameraPosition = actor.position.clone().add(new THREE.Vector3(0, 0, 1).multiplyScalar(80));
+      const cameraPosition = actor.position.clone().add(new THREE.Vector3(0, 1, 0).multiplyScalar(80));
       camera.position.copy(cameraPosition);
       camera.lookAt(cube1.getWorldPosition(new THREE.Vector3()));
       break;
